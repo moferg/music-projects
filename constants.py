754895ea047cfcd29a6c401acde3585039734295
Marshall Ferguson <fergusonmarshall0@gmail.com>
2023-03-19 15:06:42 -0400

    Added NOTES CHORDS and SCALES to constants.py

--- constants.py
+++ constants.py
@@ -1,2 +1,9 @@
 # Marshall Ferguson - 3/2023
 # Purpose: Store the constants to be used in the various scripts in this project
+
+NOTES = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D',
+         'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
+CHORDS = ['major', 'minor', 'sixth', 'minor sixth', 'six nine', 'major seventh', 'dominant seventh', 'seventh flat five', 'seventh sharp five', 'minor seventh', 'minor with a major seventh', 'minor seventh flat five',
+          'diminished seventh', 'ninth', 'ninth flat five', 'ninth sharp five', 'major ninth', 'minor ninth', 'minor eleventh', 'thirteenth', 'major thirteenth', 'minor thirteenth', 'suspended fourth']
+SCALES = ['major', 'harmonic minor', 'melodic minor', 'natural minor', 'pentatonic major', 'pentatonic minor', 'pentatonic blues',
+          'pentatonic neutral', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian', 'diatonic', 'diminished', 'dominant 7th']
