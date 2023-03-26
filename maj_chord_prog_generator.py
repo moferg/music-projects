dad16093edbacad20e6c5b9dc8fa72bfa60964ab
Marshall Ferguson <fergusonmarshall0@gmail.com>
2023-03-26 13:20:01 -0400

    Defined main() in maj_chord_prog_generator.py

--- maj_chord_prog_generator.py
+++ maj_chord_prog_generator.py
@@ -1,14 +1,18 @@
 # Marshall Ferguson - 3/2023
-# Purpose: Generate a random combination of a chord progression to practice
+# Purpose: Generate a random four-chord chord progression to practice
 
 # Imports
 import constants
 import random
 
+# Variables
+rand_maj_chord_prog = ['I']
+
 
 def main():
-    for _ in range(4):
-        print(random.choice(constants.CHORD_PROG_MAJ))
+    for _ in range(3):
+        rand_maj_chord_prog.append(random.choice(constants.CHORD_PROG_MAJ))
+    print(rand_maj_chord_prog)
 
 
 main()
