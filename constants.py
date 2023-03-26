ffa8adf23f361ccea8dadad9523cd04f7139036e
Marshall Ferguson <fergusonmarshall0@gmail.com>
2023-03-26 12:51:39 -0400

    Updated CHORDS and added CHORDS_ADV to constants.py

--- constants.py
+++ constants.py
@@ -3,7 +3,9 @@
 
 NOTES = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D',
          'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
-CHORDS = ['major', 'minor', 'sixth', 'minor sixth', 'six nine', 'major seventh', 'dominant seventh', 'seventh flat five', 'seventh sharp five', 'minor seventh', 'minor with a major seventh', 'minor seventh flat five',
-          'diminished seventh', 'ninth', 'ninth flat five', 'ninth sharp five', 'major ninth', 'minor ninth', 'minor eleventh', 'thirteenth', 'major thirteenth', 'minor thirteenth', 'suspended fourth']
+CHORDS = ['major', 'minor', 'seventh', 'fifth', 'diminshed', 'diminished seventh',
+          'augmented', 'suspended second', 'suspended fourth', 'major seventh', 'minor seventh']
+CHORDS_ADV = ['major', 'minor', 'sixth', 'minor sixth', 'six nine', 'major seventh', 'dominant seventh', 'seventh flat five', 'seventh sharp five', 'minor seventh', 'minor with a major seventh', 'minor seventh flat five',
+              'diminished seventh', 'ninth', 'ninth flat five', 'ninth sharp five', 'major ninth', 'minor ninth', 'minor eleventh', 'thirteenth', 'major thirteenth', 'minor thirteenth', 'suspended fourth']
 SCALES = ['major', 'harmonic minor', 'melodic minor', 'natural minor', 'pentatonic major', 'pentatonic minor', 'pentatonic blues',
           'pentatonic neutral', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian', 'diatonic', 'diminished', 'dominant 7th']
